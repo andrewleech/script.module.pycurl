38000d37f81f576e74cddf77b5d94b5c168d216d
Andrew Leech <andrew@alelec.net>
2015-09-23 23:03:38 +1000

    rename pycurl folder to kodi_pycurl to avoid name clash. update loader functionality

--- lib/kodi_pycurl/__init__.py
+++ lib/kodi_pycurl/__init__.py
@@ -1,23 +1,28 @@
 """
 ## Multiple platform selector for pycurl binary distributions
-# Original from: https://pypi.python.org/pypi/human_curl/0.13
+# Original from: https://pypi.python.org/pypi/pycurl/7.19.5.1
+
+## To Use:
+# in addon.xml
+<import addon="script.module.pycurl" version="7.19.5"/>
+# in your project
+import kodi_pycurl
+import pycurl
 
 ## To add new platform:
 # Ensure python compatible toolchain is installed
 # Ensure you run the same version of python used in kodi on your platform.
 
+# To build binary module for your platform:
 cd script.module.pycurl/lib
-python -m pycurl
+python -c "import kodi_pycurl; kodi_pycurl.build_binary_dist()"
 
-# This will print out the identifier matches on the current system
-# add a suitable identifier match -> new folder path below
 # Please do contribute added distributions back to project!
 """
 
 import re
 import os
 import sys
-import platform
 import string
 
 package = "pycurl"
@@ -192,20 +197,16 @@ def get_platform ():
 
 plat_name = get_platform()
 target_version = sys.version[0:3]
-
-binary_package_name = "%s-%s.%s-py%s.exe" % (package, version, plat_name, target_version)
-
+binary_package_name = "%s-%s-py%s-%s" % (package, version, target_version, plat_name)
 binary_package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), binary_package_name))
 
 if not binary_package_path or not os.path.exists(binary_package_path):
     print "%s distribution not available for your platform.\n%s \nPlease add one if possible by following instructions in top of file: %s" %(package,binary_package_name, str(__file__))
 
 else:
-    # Add required paths to python search path
-    if binary_package_path not in sys.path:
-        sys.path.append(binary_package_path)
-
-    from pycurl import *
+    # Load real module
+    import imp
+    imp.load_source(package, os.path.join(binary_package_path, 'pycurl.py'))
 
 def build_binary_dist():
     # Try to build the binary module
